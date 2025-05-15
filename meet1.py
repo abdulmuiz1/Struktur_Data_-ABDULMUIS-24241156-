4f41a01aaab0f0e742f507cc1d571e6c5550134e
abdulmuiz1 <muizabdmz@gmail.com>
2025-05-15 22:08:33 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,22 @@
+x = 24
+y= 56
+hasil = x < y
+print(hasil)
+
+
+hasil = x > y
+print(hasil)
+
+
+
+hasil = x <= y
+print(hasil)
+
+hasil = x >= y
+print(hasil)
+
+hasil = x == y
+print(hasil)
+
+hasil = x != y
+print(hasil)
